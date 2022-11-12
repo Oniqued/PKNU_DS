cffb9c5c67e0a9be344c8bf07a36cdb5e81b15e6
Oniqued <zktl3575@gmail.com>
2022-11-13 01:20:42 +0900

    [prob03] third commit

--- PKNU_DS_08/PKNU_DS_08/prob03.cpp
+++ PKNU_DS_08/PKNU_DS_08/prob03.cpp
@@ -19,26 +19,26 @@ typedef struct testCase {
 //함수 프로토타입
 void load();
 void allocation(int i);
-void search(Case *c, int index);
+void search(int i, int j, int input);
 
 //사용될 전역 변수
+int test_cases;
 Case *c[MAX];
 
 int main() {
 	clock_t start = clock();
 	load();
 	clock_t end = clock();
-	printf("elapesed time: %lf\n", (double)(end - start)/ CLOCKS_PER_SEC);
+	printf("Elapsed Time: %lf\n", (double)(end - start)/ CLOCKS_PER_SEC);
 
 	return 0;
 }
 
 void load() {
-	int test_cases;
 	int input_len;
 	int input;
 
-	FILE* fp = fopen("sample_input.txt", "r");
+	FILE* fp = fopen("input.txt", "r");
 	if (fp == NULL) {
 		return;
 	}
@@ -49,11 +49,11 @@ void load() {
 		fscanf(fp, "%d", &input_len); //해당 테스트 케이스 data의 길이는?
 		for (int j = 0; j < input_len; j++) {
 			fscanf(fp, "%d", &input); //넣으려는 값 스캔
-			search(c[i], input);
+			search(i, j, input);
 			c[i]->data[j] = input; //데이터 삽입 
-			printf("%d ", c[i]->data[j]);
+			//printf("%d ", c[i]->data[j]);
 		}
-		printf("case: %d, huristic: %d\n", i, c[i]->huristic);
+		printf("%d\n", c[i]->huristic);
 	}
 }
 
@@ -62,6 +62,13 @@ void allocation(int i) {
 	c[i]->huristic = 0; //huristic 초기화
 }
 
-void search(Case *c, int input) {
-	//if(input > )
+void search(int i, int j, int input) {
+	for(int k = j-1; k >= 0; k--){
+		if (c[i]->data[k] < input) {
+			c[i]->huristic++;
+		}
+		else {
+			break;
+		}
+	}
 }
