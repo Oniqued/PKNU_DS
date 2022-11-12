d674e1af18652cd40161081de47d9dbda5d59f38
Oniqued <zktl3575@gmail.com>
2022-11-12 23:30:34 +0900

    [prob03] second commit

--- PKNU_DS_08/PKNU_DS_08/prob03.cpp
+++ PKNU_DS_08/PKNU_DS_08/prob03.cpp
@@ -1,31 +1,43 @@
-#include "stack.h"
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+#define BUFFER_SIZE 1000000
 #define MAX 100
 
-void load();
-int search(Case c);
+//구조체 
+typedef struct testCase {
+	int data[BUFFER_SIZE];
+	int huristic;
+}Case;
 
-int test_cases;
-Case c[MAX];
+//함수 프로토타입
+void load();
+void allocation(int i);
+void search(Case *c, int index);
+
+//사용될 전역 변수
+Case *c[MAX];
 
 int main() {
+	clock_t start = clock();
 	load();
-	/*for (int i = 0; i < test_cases; i++) {
-		printf("%d\n", search(c[i]));
-	}*/
-	
-	/*for (int i = 0; i < test_cases; i++) {
-		for (int j = 0; j < c[i]->top; j++) {
-			printf("%d ", c[i]->stack[j]);
-		}
-	}*/
+	clock_t end = clock();
+	printf("elapesed time: %lf\n", (double)(end - start)/ CLOCKS_PER_SEC);
 
 	return 0;
 }
 
 void load() {
-	int index_amt;
-	int index;
+	int test_cases;
+	int input_len;
+	int input;
+
 	FILE* fp = fopen("sample_input.txt", "r");
 	if (fp == NULL) {
 		return;
@@ -33,24 +45,23 @@ void load() {
 
 	fscanf(fp, "%d", &test_cases); //테스트 케이스 갯수 스캔
 	for (int i = 0; i < test_cases; i++) { //테스트 케이스 갯수 만큼 반복
-		create(); //테스트 케이스 생성
-		fscanf(fp, "%d", &index_amt); //해당 테스트 케이스 stack의 길이는?
-		c[i]->top = index_amt - 1; //c[i]->stack의 top 초기화
-		for (int j = 0; j <= c[i]->top; j++) {
-			fscanf(fp, "%d", &index); //넣으려는 값 스캔
-			push(c[i], index); //스캔 한 값 차례로 stack에 적재
+		allocation(i); //메모리 할당
+		fscanf(fp, "%d", &input_len); //해당 테스트 케이스 data의 길이는?
+		for (int j = 0; j < input_len; j++) {
+			fscanf(fp, "%d", &input); //넣으려는 값 스캔
+			search(c[i], input);
+			c[i]->data[j] = input; //데이터 삽입 
+			printf("%d ", c[i]->data[j]);
 		}
+		printf("case: %d, huristic: %d\n", i, c[i]->huristic);
 	}
 }
 
-int search(Case c) {
-	int count = 0;
-	for (c->top; c->top >= 0; c->top--) {
-		for (int i = c->top - 1; i >= 0; i--) {
-			if (c->top >= i) {
-				count++;
-			}
-		}
-	}
-	return count;
+void allocation(int i) {
+	c[i] = (Case*)malloc(sizeof(Case)); //테스트 케이스 메모리 공간 생성
+	c[i]->huristic = 0; //huristic 초기화
+}
+
+void search(Case *c, int input) {
+	//if(input > )
 }
