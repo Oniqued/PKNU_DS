cce8f781f084d81fe0632d518ed71c741e4b1294
Oniqued <zktl3575@gmail.com>
2022-11-22 14:27:33 +0900

    [prob02] first commit

---
+++ PKNU_DS_10/PKNU_DS_10/prob02.cpp
@@ -0,0 +1,21 @@
+//테스트 결과 데이터 출력 성공
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int compare(char* str1, char* str2);
+
+int main() {
+	char str1[MAX], str2[MAX];
+	scanf("%s %s2", str1, str2);
+
+}
+
+int compare(char* str1, char* str2) {
+	
+	if (str1[] < str2[]) {
+		return compare(str1, str2);
+	}
+}
